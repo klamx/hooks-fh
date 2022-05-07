b02100f835bf489266f6584b70c28bca03d18d2d
Alejandro Largo <largoalejo@gmail.com>
2022-05-07 16:01:32 -0500

    useCounter custom hook

--- src/HookApp.jsx
+++ src/HookApp.jsx
@@ -1,7 +1,7 @@
 const HookApp = () => {
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <h1>Hola Mundo!</h1>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HookApp from './HookApp'
+// import HookApp from './HookApp'
+// import CounterApp from './components/01-useState/CounterApp';
+import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HookApp />)
+root.render(<CounterWithCustomHook />)
